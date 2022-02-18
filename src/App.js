b1c121bf9bed31c264066c5b35f73a5911067e2b
Zahra Elair <zahraelair17@gmail.com>
2022-02-18 21:00:13 +0100

    version 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import SideBar from "./components/SideBar";
+import Body from "./components/Body";
+import { drawChart, randomArray } from "./functions/Tools";
+const App = () => {
+  const arrayLength = 15;
 
-function App() {
+  const [Type, setType] = useState("");
+  const [speed, setSpeed] = useState(1000);
+  const [array, setArray] = useState();
+
+  useEffect(() => {
+    setArray(randomArray(arrayLength, 100));
+    drawChart(array, arrayLength);
+  }, [Type]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar setType={setType} setSpeed={setSpeed} />
+      {Type && <Body Type={Type} speed={speed} array={array} />}
     </div>
   );
-}
+};
 
 export default App;
